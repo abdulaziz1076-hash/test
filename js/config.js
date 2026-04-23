06a3ad5df1f834dd9fc6d2492a40bacf34de303e
abdulaziz1076-hash <abdulaziz1076@gmail.com>
2026-04-23 14:02:07 +0400

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,6 @@
+// تكوين التطبيق
+export const APP_CONFIG = {
+  siteName: { ar: "سوق المشاريع", en: "Souq Almasharie" },
+  supabaseUrl: "https://xyepcwptfihnztgghimx.supabase.co",
+  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh5ZXBjd3B0Zmlobnp0Z2doaW14Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY0ODA2OTcsImV4cCI6MjA5MjA1NjY5N30.SZvZjWpih1JiUlv24xLhNwjTvzo9RNsrTiI9fchWldE"
+};
